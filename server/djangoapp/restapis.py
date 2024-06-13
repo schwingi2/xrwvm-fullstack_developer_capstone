64e78a24f6921b0637dd3523c4d8ce9172b6e2e4
Albert Schwingenschlögl <46405059+schwingi2@users.noreply.github.com>
2024-06-13 21:44:22 +0200

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -4,22 +4,25 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-#OPEN ISSUE BACKEND URL IS RETRIEVED AS https://albertschwin-3030.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai
+# OPEN ISSUE BACKEND URL IS RETRIEVED AS https://albertschwin-3030.
+# theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai
 backend_url = os.getenv(
     'backend_url', default="http://localhost:3030")
 
-#backend_url="https://albertschwin-3030.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"   
+# backend_url="https://albertschwin-3030.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"
 sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
 # def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
+
+
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+    if (kwargs):
+        for key, value in kwargs.items():
+            params = params+key+"="+value+"&"
 
     request_url = backend_url+endpoint+"?"+params
     print("BackendURL: {}".format(backend_url))
@@ -33,6 +36,8 @@ def get_request(endpoint, **kwargs):
         print("Network exception occurred")
 
 # def analyze_review_sentiments(text):
+
+
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
     print(f"Sentiment Analyzer URL{request_url}")
@@ -47,10 +52,12 @@ def analyze_review_sentiments(text):
 # Add code for retrieving sentiments
 
 # def post_review(data_dict):
+
+
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
     except:
